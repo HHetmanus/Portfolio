25b741404f8008e0f8892c63232bddb535759793
HHetmanus <111887858+HHetmanus@users.noreply.github.com>
2023-03-18 07:53:35 +0000

    Create logo.js

---
+++ JavaScript/logo.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+            var i = 0, logo;
+            logo = "Portfolio."
+            
+            function typing() {
+                if(i < logo.length){
+                document.getElementById("logo").innerHTML += logo.charAt(i);
+                i++;
+                setTimeout(typing, 150);
+                }
+            
+            }
+            typing();
+            
+            
+        </script>
